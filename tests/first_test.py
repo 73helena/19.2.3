283b77a7d98f6b7f8e3d9f79dd9e4787beba0840
73helena <101932825+73helena@users.noreply.github.com>
2022-06-13 16:53:05 +0300

    Update first_test.py

--- tests/first_test.py
+++ tests/first_test.py
@@ -9,9 +9,6 @@ class TestCalc:
     def test_multiply_calculate_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
-    # def test_multiply_calculation_failed(self):
-    #     assert self.calc.multiply(self, 2, 2) == 5
-
     def test_division_calculate_correctly(self):
         assert self.calc.division(self, 4, 2) == 2
 
